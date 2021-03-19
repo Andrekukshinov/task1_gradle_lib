dab0d829a25773ce60129587e10377af3471ba5d
Андрей <andrekukshinov@gmail.com>
2021-03-19 15:39:33 +0300

    string utils ready with tests

---
+++ lib/src/test/java/com/epam/utils/utils/StringUtilsTest.java
@@ -0,0 +1,33 @@
+package com.epam.utils.utils;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StringUtilsTest {
+
+    private static Stream<Arguments> dataProvider() {
+        return Stream.of(
+                Arguments.of("1", true),
+                Arguments.of("10", true),
+                Arguments.of("0", false),
+                Arguments.of("-1", false),
+                Arguments.of("", false),//refactor for exceptions
+                Arguments.of(" ", false)//refactor for exceptions
+                );
+    }
+
+    @ParameterizedTest
+    @MethodSource("dataProvider")
+    void testIsPositiveNumber(String value, boolean expected) {
+        boolean actual = StringUtils.isPositiveNumber(value);
+
+        Assertions.assertEquals(expected, actual);
+    }
+}
